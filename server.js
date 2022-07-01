9f87151f7b7bfd44651867faa849201693212ab1
Gabriel <santosdalbelo@gmail.com>
2022-07-01 15:53:01 -0300

    Update server.js

--- server.js
+++ server.js
@@ -20,18 +20,14 @@ mongoose.connect(process.env.DB_CONNECTION, {useNewUrlParser: true, useUnifiedTo
 })
 
 server.listen(3000, () => {
-    let cashback = {porcentagem: 0, valor: 0};
-    calcCashback(cashback, 350.99);
-    console.log(cashback.valor > 1500, cashback.valor);
-    console.log((cashback.valor <= 1000.00), cashback.valor);
-    console.log('Valor1: ',cashback.valor, cashback.porcentagem);
-    calcCashback(cashback, 1502.00);
-    console.log(cashback.valor > 1500, cashback.valor);
-    console.log((cashback.valor > 1000 && cashback.valor < 1500), cashback.valor);
-    console.log('Valor2: ',cashback.valor, cashback.porcentagem);
+    let cashback = {cashback: 0, valor: 0};
+    console.log('Cashback1: ' + calcCashback(350).toFixed(2));
+    console.log('Valor1: ',calcCashback(350).toFixed(2), '10%', 350);
     calcCashback(cashback, 1350.99);
-    console.log(cashback.valor > 1500, cashback.valor);
-    console.log((cashback.valor > 1000 && cashback.valor < 1500), cashback.valor);
-    console.log('Valor3: ',cashback.valor, cashback.porcentagem);
+    console.log('Cashback3: ' + calcCashback(1240).toFixed(2));
+    console.log('Valor3: ',calcCashback(1240).toFixed(2), '15%', 1240);
+    calcCashback(cashback, 1502.00);
+    console.log('Cashback2: ' + calcCashback(2300.40).toFixed(2));
+    console.log('Valor2: ',calcCashback(2300.40).toFixed(2), '20%', 2300.40);
     console.log("Server running on port 3000");
 })
